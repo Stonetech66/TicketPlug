9361bb53475404d905c922bbd4e8327ec5e7ed22
Livingstone Maxwell <106387735+Stonetech66@users.noreply.github.com>
2023-03-25 17:51:23 +0100

    Update urls.py

--- Events/urls.py
+++ Events/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
 urlpatterns= [ 
-    path('', views.EventView.as_view(), name='all-events'),
+    path('events/', views.EventView.as_view(), name='all-events'),
     path('create/event/', views.EventCreateView.as_view()),
-    path('<uuid:pk>/', views.EventDetailView.as_view(), name='event-detail'),
-    path('buy-ticket/', views.BuyTicketView.as_view(), name='buy-ticket'),
+    path('event/<uuid:pk>/', views.EventDetailView.as_view(), name='event-detail'),
+    path('buy-ticket/<int:event>/', views.BuyTicketView.as_view(), name='buy-ticket'),
     path('create/event-ticket/', views.CreateEventTicketPrice.as_view()),
     path('categories/', views.CategoryView.as_view()), 
     path('update-trending-events/', views.TrendingEventView.as_view()),
-    path('trending-events/', views.TrendingEventss.as_view())
+    path('trending-events/', views.TrendingEventss.as_view()),
+    path('remove-ticket/<int:pk>', views.RemoveTicketView.as_view())
+
 ]
