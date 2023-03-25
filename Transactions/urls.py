d5733167bce33e9bf45ea5aa5e7616a70ac9157b
Livingstone Maxwell <106387735+Stonetech66@users.noreply.github.com>
2023-03-25 17:41:05 +0100

    Update urls.py

--- Transactions/urls.py
+++ Transactions/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns=[ 
     # path('withdraw/', views.WithdrawView.as_view()),
-    path('make-payment/<uuid>/', views.Makepayment.as_view(), name='payment'),
+    path('make-payment/<uuid:pk>/', views.Makepayment.as_view(), name='payment'),
     path('paystack-webhook/', views.PayStack_Webhook.as_view(), name='paystack-webhook'),
+    path('verify-qrcode/<uuid:event>/<int:key>/',views.VerifyQrcode.as_view(), name='verify-qrcode')
     # path('complete-withdrawal/<uuid>/', views.CompleteWithdrawal)
 ]
