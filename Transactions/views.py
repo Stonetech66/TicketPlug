7a8b00bd04bfbb1d2e7c21fdcc8553c40d403582
Livingstone Maxwell <106387735+Stonetech66@users.noreply.github.com>
2023-03-25 17:39:37 +0100

    Update views.py

--- Transactions/views.py
+++ Transactions/views.py
@@ -1,15 +1,15 @@
 from Events.models import OrderTicket, BuyTicket
 from rest_framework import generics, status
-from .serializers import PaymentSErializer
+from .serializers import PaymentSErializer, CheckoutSerializer
 from rest_framework.views import APIView
-from ADMIN.models import AdminSetting
 import requests
 from django.conf import settings
-from .tasks import Create_PaymentRecord
+from .tasks import Create_PaymentRecord, PaymentFailed
 from rest_framework.response import Response
 from rest_framework.response import Response
-# Create your views here.
 
+# Create your views here.
+from . models import SoldTicket
 
 class Makepayment(APIView):
     '''
@@ -19,9 +19,19 @@ class Makepayment(APIView):
     def get_serializer(self):
         return PaymentSErializer()
 
+    def get(self , *args, **kwargs):
+        user=self.request.user
+        id= self.kwargs["pk"]
+        try:
+          order=OrderTicket.objects.get(id=id, user=user, status='not completed')
+          return Response(CheckoutSerializer(order).data)
+
+        except:
+             return Response({"error":" invalid id or you dont have any tickets for this event"}, status=status.HTTP_400_BAD_REQUEST)
+
     def post(self, *args, **kwargs):
         serializer=self.serializer_class(data=self.request.data)
-        p=self.kwargs['uuid']
+        p=self.kwargs['pk']
         j=OrderTicket.objects.filter(user=self.request.user, id=p, status='not completed')
         if j.exists():
             if serializer.is_valid(raise_exception=True):
@@ -30,12 +40,16 @@ class Makepayment(APIView):
                 user=str(self.request.user.id)
                 email=serializer.validated_data.get('email')
                 u_e=str(self.request.user.email)
-                url='https://api.paystack.co/transaction/initialize'
-                header={'authorization': f'Bearer {settings.PAYSTACK_SECRET_KEY}'}
-                try:
-                    resp=requests.post(url, timeout=100, headers=header, json={'amount':price*100, 'email':u_e, 'metadata':{'user_id':user, 'bought_ticket_id':p, 'price':price,'email':email, },})
+                if price == 0:
+                     Create_PaymentRecord(amount=price, email=email, user_id=user, bought_ticket_id=p)
+                     return Response({"message" :"checkout successful, check your email to find your tickets"}) 
+                else:    
+                  url='https://api.paystack.co/transaction/initialize'
+                  header={'authorization': f'Bearer {settings.PAYSTACK_SECRET_KEY}'}
+                  try:
+                    resp=requests.post(url, timeout=100, headers=header, json={'amount':price*10, 'email':u_e, 'metadata':{'user_id':str(user), 'bought_ticket_id':str(p), 'price':price,'email':email, },})
                     return Response(resp.json())
-                except Exception as e:
+                  except Exception as e:
                     return Response({'error': f'{e}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         return Response({'error':'invalid uuid or you have not selected a ticket yet'}, status=status.HTTP_404_NOT_FOUND)
 
@@ -52,39 +66,27 @@ class PayStack_Webhook(APIView):
             bought_ticket=c['data']['metadata']['bought_ticket_id']
             price=c['data']['metadata']['price']
             email=c['data']['metadata']['email']
-            print(bought_ticket)
             Create_PaymentRecord(amount=price, email=email, user_id=user_id, bought_ticket_id=bought_ticket)
-            return Response({'message':'ok'},status=status.HTTP_200_OK)
+
+        elif c['event'] =='charge.failed':
+            user_id=c['data']['metadata']['user_id']
+            bought_ticket=c['data']['metadata']['bought_ticket_id']
+            price=c['data']['metadata']['price']
+            email=c['data']['metadata']['email']
+            PaymentFailed(amount=price, email=email, user_id=user_id, bought_ticket_id=bought_ticket)
+
+            
         return Response(status=status.HTTP_200_OK)
 
-# class WithdrawView(APIView):
-#     serializer_class=WithdrawSerializer
-
-#     def post(self, request,*args, **kwrargs):
-#         serializer=WithdrawSerializer(data=request.data)
-#         minimum_withdrawal=AdminSetting.objects.last().minimum_withdrawal
-#         wallet=request.user.get_wallet_balance() 
-#         if wallet< minimum_withdrawal:
-#             return Response({'error':'Insufficient balance'})
-#         if serializer.is_valid(raise_exception=True):
-#             amount=serializer.validated_data.get('amount')
-#             if amount > wallet:
-#                 return Response({'error':'insufficient balance your requested amount is greater than your wallet balance'})
-#             else:
-#                 p=Withdrawal.objects.create(user=request.user, amount=amount)
-#                 return Response({'withdrawal-link'})
-
-
-# class CompleteWithdrawal(APIView):
-#     def post(self, request,*args, **kwargs):
-#         uid=self.kwargs['uuid']
-#         user=request.user.id
-#         w=Withdrawal.objects.filter(uid=uid, user=request.user, status='requested')
-#         if w.exists():
-#             # url= 
-#             # headers=
-#             # resp=requests.post(url, headers=headers, json={})
-#             #return Response(resp.json())
-        
 
     
+class VerifyQrcode(APIView):
+    permission_classes=[] 
+    def get(self, *args, **kwargs):
+        event=self.kwargs['event']
+        key= self.kwargs['key']
+        try:
+            ticket= SoldTicket.objects.get(event__id=event, key=key, is_used=False)
+            return Response({'valid':True})
+        except:
+            return Response({'valid':False} , status=status.HTTP_400_BAD_REQUEST) 
