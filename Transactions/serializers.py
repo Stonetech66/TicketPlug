a2ed8c5e8a577e45f5181544b1a3911c3beffe73
Livingstone Maxwell <106387735+Stonetech66@users.noreply.github.com>
2023-03-25 17:42:01 +0100

    Update serializers.py

--- Transactions/serializers.py
+++ Transactions/serializers.py
@@ -5,5 +5,13 @@ from rest_framework import serializers
 class PaymentSErializer(serializers.Serializer):
     email=serializers.EmailField(required=False)
     
+class TicketSerializer(serializers.Serializer):
+    ticket=serializers.CharField(read_only=True, source='event_ticket.label')
+    qty=serializers.IntegerField(read_only=True)
+    total=serializers.FloatField(read_only=True, source="get_total_ticket_price")
+class CheckoutSerializer(serializers.Serializer):
+    event= serializers.CharField(source="event.name", read_only=True)
+    tickets= TicketSerializer(read_only=True, many=True,)
+    total= serializers.FloatField(source="get_total_price", read_only=True)
 # class WithdrawSerializer(serializers.Serializer):
 #     amount=serializers.FloatField()
