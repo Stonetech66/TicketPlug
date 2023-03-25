0cfdc10925a61591c4aed939d746a55857c927d6
Livingstone Maxwell <106387735+Stonetech66@users.noreply.github.com>
2023-03-25 17:49:46 +0100

    Update views.py

--- Events/views.py
+++ Events/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from django.urls import reverse
 from django.core.exceptions import ValidationError
 from django_filters.rest_framework import DjangoFilterBackend
-
+import uuid
 
 
 
@@ -59,54 +59,56 @@ class EventDetailView(generics.RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         return Event.objects.all().select_related('user').prefetch_related('event_fees', 'category')
 
-
+class RemoveTicketView(APIView):
+    def post(self, *args, **kwargs):
+        pk=self.kwargs["pk"]
+        try:
+            ticket=TicketPrice.objects.get(id=pk)
+            order=OrderTicket.objects.get(user=self.request.user, status= "not completed", event=ticket.event)
+            BuyTicket.objects.delete(id=pk, completed=False, order=order)
+            return Response({"message": "success"}, status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response({"message":"ticket dosent exists or ticket not added"}, status=status.HTTP_400_BAD_REQUEST)
 class BuyTicketView(APIView):
-    '''
-    (*important A GET request must be made first when the user wants to buy a ticket for a event, it should be made only once and with a query parameter event= id of the event the user wants to buy e.g ( `buy-ticket/?event=(id of the evnt the user wants to buy` )))
-    (POST: endpoint to buy a ticket  Note:  if a user decides to send the ticket to more than one email, the emails should be put in a list  and added to the emails field else if the user picks one email the email should be put in this endpoint endpoint(make-payment/)
-    ( *fields:  ticket_id: ( ` id of the event ticket that the user wants to buy` ), qty: ( `quantity of the ticket the user wants to buy should be an integer` ), emails: (` a list of the emails the user wants to send the tickets to can be left blank` ) )
-    '''
+
     serializer_class= BuyTicketSerializer
 
     def get_serializer(self):
         return BuyTicketSerializer()
 
-    def get(self, request, *args, **kwargs):
-        event=str(request.GET.get('event'))
-        if event == None:
-            return Response({'error':'You must include the event query in your get request which must contain the id of the event'})
-        try:
-            p=OrderTicket.objects.filter(user=request.user, event__id=event ,status='not completed')
-        except ValidationError:
-            return Response({"message":'invalid event uid'}, status=status.HTTP_400_BAD_REQUEST)
-        if p.exists():
-            p=p.last()
-            p.tickets.clear()
-            return Response({'message':'user has incomplete order'})
-        return Response({"message":'no incomplete order'})
-
     def post(self, request, *args, **kwargs):
 
         serializer=self.serializer_class(data=request.data)
         if serializer.is_valid(raise_exception=True):
-                user=self.request.user
-                event_ticket=serializer.validated_data.get('event_ticket')
-                print(event_ticket)
-                qty=serializer.validated_data.get('qty')
-                emails=serializer.validated_data.get('emails')
-                if emails != None and len(emails) != qty:
-                    return Response({'emails':'incomplete emails provided, qty and emails dont match'}, status=status.HTTP_400_BAD_REQUEST)
-                p=TicketPrice.objects.filter(id=event_ticket)
-                if p.exists():
-                    c=BuyTicket.objects.create(user=user, event_ticket=p.last(), qty=qty, emails=str(emails))
+                    user=self.request.user
+                # if user == None:
+                    event_ticket=self.kwargs["event"]
+                    qty=serializer.validated_data.get('qty')
+                    emails=serializer.validated_data.get('emails')
                     try:
-                        p=OrderTicket.objects.get(user=request.user,event=c.event_ticket.event, status='not completed' )
+                        ticket=TicketPrice.objects.get(id=event_ticket)
+                        if ticket.get_tickets_available() == 0:
+                            return Response({'error':'Ticket sold out'}, status=status.HTTP_400_BAD_REQUEST)
+                        elif ticket.get_tickets_available() < qty:
+                            return Response({'error':'Tickets available not up to this qty'}, status=status.HTTP_400_BAD_REQUEST)
+
                     except:
-                        p=OrderTicket.objects.create(user=request.user, event=c.event_ticket.event)
-                    p.tickets.add(c)
-                    p.save()
-                    return Response({"payme-link":reverse('payment', kwargs={'uuid':p.id}), 'amount_to_pay':p.get_total_price()})
-                return Response({'error':'invalid ticket id entered'}, status=status.HTTP_400_BAD_REQUEST)
+                        return Response({'error':'This ticket cant be found'}, status=status.HTTP_404_NOT_FOUND)
+                    order, created=OrderTicket.objects.get_or_create(user=request.user, status='not completed' , event=ticket.event)
+                    try:
+                        c=BuyTicket.objects.get(user=user, event_ticket=ticket, completed=False, order=order)
+                        c.qty=qty
+                        c.emails=emails
+                        c.save()
+                    except:
+                        c=BuyTicket.objects.create(user=user, event_ticket=ticket, qty=qty, emails=emails, order=order)
+                    return Response({"payment-link":reverse('payment', kwargs={'pk':order.id}), 'amount_to_pay':order.get_total_price()})
+                # else:
+                #     cookie=request.COOKIE.get('AnonymousOrder')
+                #     if cookie == None or cookie=='':
+                #         o=OrderTicket.objects.create(id= uuid.uui4(),  )
+
+
 
 
 
@@ -128,8 +130,11 @@ class TrendingEventss(generics.ListAPIView):
     permission_classes=[]
 
     def get_queryset(self):
-        return TrendingEvents.objects.prefetch_related('events').last().events.all().select_related('user').prefetch_related('event_fees')
-
+        try:
+  
+           return TrendingEvents.objects.prefetch_related('events').last().events.all().select_related('user').prefetch_related('event_fees')
+        except:
+            return [] 
 
 class TrendingEventView(generics.UpdateAPIView):
     '''
