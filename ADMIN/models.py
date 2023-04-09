bdb3d83a942f90ecd3bcdefc9c6e6270e2515ae0
Livingstone Maxwell <106387735+Stonetech66@users.noreply.github.com>
2023-04-09 20:18:27 +0100

    Update models.py

--- ADMIN/models.py
+++ ADMIN/models.py
@@ -3,7 +3,5 @@ from django.db import models
 # Create your models here.
 class AdminSetting(models.Model):
     header_image=models.ImageField(upload_to='media/', null=True, blank=True)
-    header_image1=models.ImageField(upload_to='media/', null=True, blank=True)
-    header_image2=models.ImageField(upload_to='media/', null=True, blank=True)
     payment_charge=models.FloatField(blank=True, null=True)
     
